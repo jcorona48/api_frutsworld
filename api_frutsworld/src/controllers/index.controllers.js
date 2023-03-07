2bec0b28fca3761112ed9ffdf050b796cf642440
Pesteban018 <107761268+Pesteban018@users.noreply.github.com>
2023-03-07 16:57:42 +0100

    Update index.controllers.js

--- api_frutsworld/src/controllers/index.controllers.js
+++ api_frutsworld/src/controllers/index.controllers.js
@@ -1,10 +1,5 @@
 import { pool } from '../db.js'
 
 export const ping = async (req, res) => {
-<<<<<<< Updated upstream
-    const [result] = await pool.query('SELECT "pong" AS result')
-=======
-    const [result] = await pool.query("SELECT 'pong' AS result")
->>>>>>> Stashed changes
     res.json(result[0])
 }
